7968b75f8bfff6ec95112f690ad17345c8486443
oONinaOo <loveninagold@gmail.com>
2017-06-15 14:16:51 +0200

    added exercise1 / task4

---
+++ LookupCollections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace LookupCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+            list["Estados Unidos"] = "United States of America";
+            list["Canadá"] = "Canada";
+            list["España"] = "Spain";
+
+            Console.WriteLine(list["españa"]);
+            Console.WriteLine(list["CANADÁ"]);
+            Console.Read();
+        }
+    }
+}
